544c1a27b2a08ae698fd09bd6cbe9ff0749bbc8d
Kazumune Katagiri <kkaz123@gmail.com>
2026-02-07 19:33:46 +0900

    /oauth/token endpoint 実装

--- app.js
+++ app.js
@@ -6,25 +6,42 @@ const port = 3000
 
 app.use(cookieParser())
 app.set('view engine', 'ejs');
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.render('index', { text: 'ejsテスト'});
 });
 
+// ↓ 本来は IdP 側のエンドポイント
 app.get('/oauth/authorize', (req, res) => {
   res.cookie('sessionId', crypto.randomUUID(), {
     httpOnly: true,
     sameSite: "lax"
   });
+  console.log(`/oauth/authorize query params:`);
+  console.log(req.query)
   res.render('oauth/authorize');
 })
 
 app.post('/oauth/login', (req, res) => {
+  console.log(`/oauth/login cookies:`);
   console.log(req.cookies);
   res.redirect(`myapp://net.jp.vss.flutter-auth/auth-callback?code=${crypto.randomUUID()}`);
+});
+
+// ↑ 本来は IdP 側のエンドポイント
+
+app.post('/oauth/token', (req, res)=>{
+  console.log(`/oauth/token request parameters:`);
+  console.log(req.body);
+  // 本来は、パラメータを元に Token エンドポイントを呼び出し、IdP からトークンを取得し、
+  // BFF 固有のトークンを作成する
+  res.json({
+    bffToken:`BFF-${crypto.randomUUID()}`
+  });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
-
